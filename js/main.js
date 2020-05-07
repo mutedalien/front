e06f3f2415a9f96a78e605f83c3511ab4efa57bc
mutedalien <chel_c@mail.ru>
2020-05-07 17:00:07 +0300

    07_05_2020

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(function(){
 
-
+$('.slider__inner').slick({ // включаем слайдер
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinity: false
+}); 
+    
 
 
 
